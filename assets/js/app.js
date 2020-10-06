e5ace81260d50a49a7d2b33a253199c27d28b3b1
Stambaugh <astambau@916schools.org>
2020-10-05 21:45:48 -0500

    chart working

---
+++ assets/js/app.js
@@ -0,0 +1,78 @@
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+    top: 20,
+    right: 40,
+    bottom: 100,
+    left: 50
+  };
+  
+  var width = svgWidth - margin.left - margin.right;
+  var height = svgHeight - margin.top - margin.bottom;
+
+//Create an SVG wrapper
+var svg = d3.select("#scatter")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+//Import Data
+d3.csv("/assets/data/data.csv").then(function(censusData) {
+    
+    //Parse data/cast as numbers
+    censusData.forEach(function(data) {
+        data.poverty = +data.poverty;
+        data.healthcare = +data.healthcare;
+    });
+
+    //Create scale functions
+    var xLinearScale = d3.scaleLinear()
+        .domain([8, d3.max(censusData, d => d.poverty)])
+        .range([0, width]);
+
+    var yLinearScale = d3.scaleLinear()
+        .domain([0, d3.max(censusData, d => d.healthcare)])
+        .range([height, 0]);
+
+    //Create axis functions
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale); 
+
+    //Append axes to the chart
+    chartGroup.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(bottomAxis);
+
+    chartGroup.append("g")
+    .call(leftAxis);
+
+    //Create circles
+    var circlesGroup = chartGroup.selectAll("circle")
+    .data(censusData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d.poverty))
+    .attr("cy", d => yLinearScale(d.healthcare))
+    .classed("stateCircle", true)
+    .attr("r", "12");
+
+    chartGroup.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .attr("class", "aText")
+    .text("Lacks Healthcare(%)");
+
+  chartGroup.append("text")
+    .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+    .attr("class", "aText")
+    .text("In Poverty (%)");
+
+}).catch(function(error) {
+    console.log(error);
+})
